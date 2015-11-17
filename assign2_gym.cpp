3680cd86791ce131f4a1b21c3ef1b21049831605
ZacBroitman <zbroitman@gmail.com>
2015-11-16 16:25:19 -0800

    Added in prototype design pattern and client code

--- assign2_gym.cpp
+++ assign2_gym.cpp
@@ -13,7 +13,12 @@ public:
 		size = area;
 		powerConsumption = power;
 	};
-	virtual GymMachine* clone();
+
+	GymMachine(const GymMachine* other){
+		powerConsumption = other->powerConsumption;
+		size = other->size;
+	}
+	//virtual GymMachine* clone();
 
 };
 
@@ -26,9 +31,15 @@ public:
 		cout << "In Rowing Machine Constructor! \n";
 		resistance = resist;
 	}
+	RowingMachine(const RowingMachine* other): GymMachine(other){
+		cout << "In Rowing Machine copy constructor \n";
+		//powerConsumption = other->powerConsumption;
+		//size = other->size;
+	}
 
-	RowingMachine* clone(){
-
+	RowingMachine* clone(int resist){
+		cout << "In Rowing Machine clone \n";
+		return new RowingMachine(this);
 	}
 };
 
@@ -41,9 +52,15 @@ public:
 		cout << "In Treadmill Constructor! \n";
 		maxSpeed = speed;
 	}
+	Treadmill(const Treadmill* other): GymMachine(other){
+		cout << "In Treadmill copy constructor \n";
+		//powerConsumption = other->powerConsumption;
+		//size = other->size;
+	}
 
-	Treadmill* clone(){
-		
+	Treadmill* clone(int speed){
+		cout << "In Treadmill clone \n";
+		return new Treadmill(this);
 	}
 
 };
@@ -54,22 +71,65 @@ public:
 Treadmill* tread = new Treadmill(0);
 RowingMachine* row = new RowingMachine(0);
 
+RowingMachine* createRowingMachine(int resist){
+	RowingMachine* tmp = row->clone(resist);
+	tmp->resistance = resist;
+	return tmp;
+}
+
+Treadmill* createTreadmill(int speed){
+	Treadmill* tmp = tread->clone(speed);
+	tmp->maxSpeed = speed;
+	return tmp;
+}
+
 int main(){
+
+	//testing code
 	cout << "In client code! \n";
 	cout << "Trying to test creating a Treadmill object \n";
 
-	Treadmill* tread = createTreadmill(20);
-	cout << "Made Treadmill with max speed: " << tread->maxSpeed << "\n";
-	cout << "Treadmill power is: " << tread->powerConsumption << "\n";
+	Treadmill* test = createTreadmill(20);
+	cout << "Made Treadmill with max speed: " << test->maxSpeed << "\n";
+	cout << "Treadmill power is: " << test->powerConsumption << "\n";
+
+	Treadmill * test2 = createTreadmill(30);
+	cout << "Made Treadmill with max speed: " << test2->maxSpeed << "\n";
+	cout << "Treadmill power is: " << test2->powerConsumption << "\n";
+
+	//assignment code
+	cout << "Here you can add more gym machine's to your gym \n";
+	cout << "Type in 'Treadmill' to add in a new Treadmill, \n";
+	cout << "Type in 'Rowing' to add in a new Rowing Machine, \n";
+	cout << "Or, type in 'exit' to finish adding new machines \n";
+	
+	string machineType;
+	cin >> machineType;
+	//Loops until user enters in 'exit'
+	while(machineType.compare("exit")!=0){
+		if(machineType.compare("Treadmill")==0){
+			int speed;
+			cout << "What is the max speed of the Treadmill?: \n";
+			cin >> speed;
+			Treadmill* tmp = createTreadmill(speed);
+		}
+		else if(machineType.compare("Rowing")==0){
+			int resist;
+			cout << "What is the resistance of the Rowing Machine?: \n";
+			cin >> resist;
+			RowingMachine* tmp = createRowingMachine(resist);
+		}
+		else{
+			cout << "Please type one of the following: \n";
+			cout << "'Treadmill' to add a new Treadmill \n";
+			cout << "'Rowing' to add a new Rowing Machine \n";
+			cout << "'exit' to finish adding machines \n\n";
+		}
+		cout << "Your last entry was: " << machineType << "\n";
+		cout << "What would you like to add next?: \n";
+		cin >> machineType;
+	}
 
 	return 0;
 
-}
-
-RowingMachine* createRowingMachine(resist){
-	return row.clone(resist);
-}
-
-Treadmill* createTreadmill(speed){
-	return tread.clone(speed);
 }
